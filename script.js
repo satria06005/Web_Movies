b2b581fda876e528e12ff991e259833d855dfb1e
satria <satrianuralim17@gmail.com>
2025-01-21 09:10:42 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,180 @@
+// 1. Jquery
+// $(".search-button").on("click", function () {
+//   $.ajax({
+//     url:
+//       "http://www.omdbapi.com/?apikey=20aeda12&s=" + $(".input-keyword").val(),
+//     success: (results) => {
+//       const movies = results.Search;
+//       let cards = "";
+//       movies.forEach((m) => {
+//         cards += showCards(m);
+//       });
+//       $(".movie-container").html(cards);
+
+//       // Ketika tombol detail di klik
+//       $(".modal-detail-button").on("click", function () {
+//         $.ajax({
+//           url:
+//             "http://www.omdbapi.com/?apikey=20aeda12&i=" +
+//             $(this).data("imdbid"),
+//           success: (m) => {
+//             const movieDetail = showMovieDetail(m);
+//             $(".modal-body").html(movieDetail);
+//           },
+//           error: (e) => {
+//             console.log(e.responseText);
+//           },
+//         });
+//       });
+//     },
+//     error: (e) => {
+//       console.log(e.responseText);
+//     },
+//   });
+// });
+
+// 2. Fetch
+// const searchButton = document.querySelector(".search-button");
+// searchButton.addEventListener("click", function () {
+//   const inputKeyword = document.querySelector(".input-keyword");
+//   fetch("http://www.omdbapi.com/?apikey=20aeda12&s=" + inputKeyword.value)
+//     .then((response) => response.json())
+//     .then((response) => {
+//       const movies = response.Search;
+//       let cards = "";
+//       movies.forEach((m) => (cards += showCards(m)));
+//       const movieContainer = document.querySelector(".movie-container");
+//       movieContainer.innerHTML = cards;
+
+//       // ketika tombol showdetails diclick
+//       const modalDetailButton = document.querySelectorAll(
+//         ".modal-detail-button"
+//       );
+//       modalDetailButton.forEach((btn) => {
+//         btn.addEventListener("click", function () {
+//           const imdbid = this.dataset.imdbid;
+//           fetch("http://www.omdbapi.com/?apikey=20aeda12&i=" + imdbid)
+//             .then((response) => response.json())
+//             .then((m) => {
+//               const movieDetail = showMovieDetail(m);
+//               const modalBody = document.querySelector(".modal-body");
+//               modalBody.innerHTML = movieDetail;
+//             });
+//         });
+//       });
+//     });
+// });
+
+// const searchButton = document.querySelector(".search-button");
+// searchButton.addEventListener("click", async function () {
+//   const inputKeyword = document.querySelector(".input-keyword");
+//   const movies = await getMovies(inputKeyword.value);
+//   updateUI(movies);
+// });
+
+// // ketika tombol showdetails diclick
+document.addEventListener("click", async function (e) {
+  if (e.target.classList.contains("modal-detail-button")) {
+    const imdbid = e.target.dataset.imdbid;
+    const movieDetail = await getMovieDetail(imdbid);
+    updateUIDetail(movieDetail);
+  }
+});
+
+// Error Handling
+const searchButton = document.querySelector(".search-button");
+searchButton.addEventListener("click", async function () {
+  try {
+    const inputKeyword = document.querySelector(".input-keyword");
+    const movies = await getMovies(inputKeyword.value);
+
+    updateUI(movies);
+  } catch (error) {
+    alert(error);
+  }
+});
+
+// ketika tombol showdetails diclick
+document.addEventListener("click", async function (e) {
+  if (e.target.classList.contains("modal-detail-button")) {
+    const imdbid = e.target.dataset.imdbid;
+    const movieDetail = await getMovieDetail(imdbid);
+    updateUIDetail(movieDetail);
+  }
+});
+
+function getMovieDetail(imdbid) {
+  return fetch("http://www.omdbapi.com/?apikey=20aeda12&i=" + imdbid)
+    .then((response) => response.json())
+    .then((m) => m);
+}
+
+function updateUIDetail(m) {
+  const movieDetail = showMovieDetail(m);
+  const modalBody = document.querySelector(".modal-body");
+  modalBody.innerHTML = movieDetail;
+}
+
+function getMovies(keyword) {
+  return fetch("http://www.omdbapi.com/?apikey=20aeda12&s=" + keyword)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      return response.json();
+    })
+    .then((response) => {
+      if (response.Response === "False") {
+        throw new Error(response.Error);
+      }
+      return response.Search;
+    });
+}
+function updateUI(movies) {
+  let cards = "";
+  movies.forEach((m) => (cards += showCards(m)));
+  const movieContainer = document.querySelector(".movie-container");
+  movieContainer.innerHTML = cards;
+}
+function showCards(m) {
+  return `
+        <div class="col-md-4 my-5">
+          <div class="card">
+            <img src="${m.Poster}" class="card-img-top" alt="" />
+            <div class="card-body">
+              <h5 class="card-title">${m.Title}</h5>
+              <h6 class="card-subtitle mb-2 text-body-secondary">${m.Year}</h6>
+              <a href="#" class="btn btn-primary modal-detail-button bg-dark" data-bs-toggle="modal"
+                    data-bs-target="#moviesDetailModal" data-imdbid="${m.imdbID}">Show Details</a>
+            </div>
+          </div>
+        </div>
+      `;
+}
+function showMovieDetail(m) {
+  return `
+            <div class="container-fluid">
+              <div class="row">
+                <div class="col-md-3">
+                  <img src="${m.Poster}" class="img-fluid" />
+                </div>
+                <ul class="list-group">
+                  <li class="list-group-item"><h4>${m.Title}</h4></li>
+                  <li class="list-group-item">
+                    <strong>Director :</strong> ${m.Director}
+                  </li>
+                  <li class="list-group-item">
+                    <strong>Actors :</strong> ${m.Actors}
+                  </li>
+                  <li class="list-group-item">
+                    <strong>Writer :</strong> ${m.Writer}
+                  </li>
+                  <li class="list-group-item">
+                    <strong>: Plot</strong> <br />
+                    ${m.Plot}
+                  </li>
+                </ul>
+              </div>
+            </div>
+          `;
+}
